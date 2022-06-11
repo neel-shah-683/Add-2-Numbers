6afbf88e789e5700096264b02b9092a963f2b043
NEEL <neelshah682003@gmail.com>
2022-06-11 18:45:48 +0530

    initial commit

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,22 @@
+a = input("Enter 1 Number: ")
+b = input("Enter 2 Number: ")
+
+# Adding 2 Numbers
+c = int(a) + int(b)
+print("\n Addition = ",c)
+
+# Subtracting 2 Numbers
+c = int(a) - int(b)
+print("\n Subtraction = ",c)
+
+# Multiplicating 2 Numbers
+c = int(a) * int(b)
+print("\n Multiplication = ",c)
+
+# Dividing 2 Numbers
+c = int(a) / int(b)
+print("\n Division = ",c)
+
+# Modulous of 2 Numbers
+c = int(a) % int(b)
+print("\n Modulous = ",c)
