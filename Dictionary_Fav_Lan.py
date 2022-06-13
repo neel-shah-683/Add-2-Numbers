03b99ca609e1cbda9f6731efc50bc832398046e0
NEEL <neelshah682003@gmail.com>
2022-06-13 21:52:14 +0530

    first commit

---
+++ Dictionary_Fav_Lan.py
@@ -0,0 +1,18 @@
+from re import A
+
+
+myDic = {
+
+}
+a = input("Enter your fav language NEEL:\n")
+b = input("Enter your fav language YASH:\n")
+c = input("Enter your fav language JATIN:\n")
+d = input("Enter your fav language SANKET:\n")
+
+myDic['NEEL'] = a
+myDic['YASH'] = b 
+myDic['JATIN'] = c 
+myDic['SANKET'] = d 
+
+print(myDic)
+
