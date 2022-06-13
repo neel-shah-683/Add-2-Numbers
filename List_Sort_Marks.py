7d3e425ad68bf7da70d9bec89c41da2eaed9c278
NEEL <neelshah682003@gmail.com>
2022-06-13 15:44:32 +0530

    first commit

---
+++ List_Sort_Marks.py
@@ -0,0 +1,10 @@
+m1 = int(input("Enter Marks1: "))
+m2 = int(input("Enter Marks2: "))
+m3 = int(input("Enter Marks3: "))
+m4 = int(input("Enter Marks4: "))
+m5 = int(input("Enter Marks5: "))
+m6 = int(input("Enter Marks6: "))
+
+MyMarks = [m1, m2, m3, m4, m5, m6]
+MyMarks.sort()
+print("Sorted List of Marks = ",MyMarks)
