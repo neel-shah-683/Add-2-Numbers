e9d86c83dd6f27fe47796d8d214317df3c5dfd80
NEEL <neelshah682003@gmail.com>
2022-06-13 15:31:39 +0530

    first commit

---
+++ List_Fruit.py
@@ -0,0 +1,14 @@
+f1 = input("Enter Fruit1: ")
+f2 = input("Enter Fruit2: ")
+f3 = input("Enter Fruit3: ")
+f4 = input("Enter Fruit4: ")
+f5 = input("Enter Fruit5: ")
+f6 = input("Enter Fruit6: ")
+f7 = input("Enter Fruit7: ")
+
+myFruits = [f1,f2,f3,f4,f5,f6,f7]
+print("\nFruits List = ",myFruits)
+
+myFruits[6]= "RASBERI" #List Can be Modified
+print("\nFruits List = ",myFruits)
+
