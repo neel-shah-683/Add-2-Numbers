43baa24d6013b977ac191299e68442c43be82e43
NEEL <neelshah682003@gmail.com>
2022-06-13 18:39:17 +0530

    first  commit

---
+++ Set_Example.py
@@ -0,0 +1,11 @@
+n1 = int(input("Enter Num1: "))
+n2 = int(input("Enter Num2: "))
+n3 = int(input("Enter Num3: "))
+n4 = int(input("Enter Num4: "))
+n5 = int(input("Enter Num5: "))
+n6 = int(input("Enter Num6: "))
+n7 = int(input("Enter Num7: "))
+n8 = int(input("Enter Num8: "))
+
+mySet = {n1, n2, n3, n4, n5, n6, n7, n8}
+print(mySet)
