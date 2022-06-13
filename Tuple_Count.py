ab59d082edf30d34b6741f45b42b8b92f7568030
NEEL <neelshah682003@gmail.com>
2022-06-13 16:03:15 +0530

    first commit

---
+++ Tuple_Count.py
@@ -0,0 +1,8 @@
+t1 = int(input("Enter Num1: "))
+t2 = int(input("Enter Num2: "))
+t3 = int(input("Enter Num3: "))
+t4 = int(input("Enter Num4: "))
+
+MyTuple = (t1, t2, t3, t4)
+
+print("Tuple Count of 0 = ",MyTuple.count(0))
