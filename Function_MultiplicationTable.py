a4fa05fd2fb016ae1931f40ee7f20ebd5e6c587e
NEEL <neelshah682003@gmail.com>
2022-06-16 17:28:32 +0530

    second commit

---
+++ Function_MultiplicationTable.py
@@ -0,0 +1,10 @@
+num = int(input("Enter Any Number: "))
+
+def table(x):
+    for i in range(1,11): #     for i in range(10 , 0,-1): TO DO REVERSE OF IT
+        print(f"{x} * {i} = {x*i}")
+
+table(num)
+
+
+
