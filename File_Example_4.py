a12918d9dadb674a49f010f32a8df47b8e8674d2
NEEL <neelshah682003@gmail.com>
2022-06-17 18:14:50 +0530

    second commit

--- File_Example_4.py
+++ File_Example_4.py
@@ -1,9 +1,11 @@
+words = ["Program"]
+
 file = open ("File_Example_4.txt")
 a = file.read()
 
-str = a.replace("Program","#")
-
-file = open("File_Example_4.txt","w")
-file.write(str)
+for word in words:
+    str1 = a.replace(word,"#")
+    file = open("File_Example_4.txt","w")
+    file.write(str1)
 
 file.close()
