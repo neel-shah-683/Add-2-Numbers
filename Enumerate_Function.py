b7baa75f24915a17d394a50780a01ba396e6daaf
NEEL <neelshah682003@gmail.com>
2022-06-28 14:51:01 +0530

    second commit

--- Enumerate_Function.py
+++ Enumerate_Function.py
@@ -1,4 +1,5 @@
 list = [2, 8, 7, 9, 4, 6, 10, 12]
 for index, item in enumerate(list):
-    if(index % 2 == 0 and (index >= 1 and index <= 7)):
-        print("Index -> ", index, "Items -> ", item)
+    if(index % 2 == 0 ):
+        if(index >= 1 and index <= 7):
+            print("Index -> ", index, "Items -> ", item)
