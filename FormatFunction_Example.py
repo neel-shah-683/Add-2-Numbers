d271779a4ba68df419994f4f44307da9d44dae4c
NEEL <neelshah682003@gmail.com>
2022-06-29 16:10:25 +0530

    first commit

---
+++ FormatFunction_Example.py
@@ -0,0 +1,6 @@
+name = input("Enter Your Name:\n")
+marks = int(input("Enter Your Marks:\n"))
+number = input("Enter Your Number:\n")
+
+result = "Your Name = {}, Marks = {} and Number = {}".format(name,marks,number)
+print(result)
